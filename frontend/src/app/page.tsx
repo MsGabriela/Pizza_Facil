e1a710a0644402d192e83edcc10378e07523e8a6
Brenno Miliani <brennomiliani@hotmail.com>
2024-02-08 21:21:11 -0300

    criacao estrutura inicial frontend

---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="bg-zinc-800 text-zinc-50 h-screen">
+      <h1 className="font-bold text-2xl">Hello World!</h1>
+    </div>
+  );
+}
