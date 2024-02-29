fef26e7ae8b1b3eeb7ffcdc755050ce4cecf585e
Brenno Miliani <brennomiliani@hotmail.com>
2024-02-29 03:38:49 -0300

    Delete frontend directory
    deletando o diretório de frontend para poder criar sem utilizar nextjs

--- frontend/src/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import Image from "next/image";
-
-export default function Home() {
-  return (
-    <div className="bg-zinc-800 text-zinc-50 h-screen">
-      <h1 className="font-bold text-2xl">Hello World!</h1>
-    </div>
-  );
-}
